7065073d13040f99ec71833654b52c116e63b4e7
shanto522 <shahriarnafij000@gmail.com>
2025-11-09 17:16:45 +0600

    middleware added

--- index.js
+++ index.js
@@ -5,8 +5,31 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
 app.use(cors());
 app.use(express.json());
+
+const verifyFireBaseToken = async (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  const token = authorization.split(" ")[1];
+  try {
+    const decoded = await admin.auth().verifyIdToken(token);
+    req.token_email = decoded.email;
+    next();
+  } catch {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9vhb7u9.mongodb.net/moviemaster?retryWrites=true&w=majority`;
 
@@ -28,9 +51,10 @@ async function run() {
       const result = await moviesCollection.find().toArray();
       res.send(result);
     });
-    app.post("/movies", async (req, res) => {
+    app.post("/movies", verifyFireBaseToken, async (req, res) => {
       const data = req.body;
-      const result = moviesCollection.insertOne();
+      const result = await moviesCollection.insertOne(data);
+      res.send(result);
     });
 
     await client.db("admin").command({ ping: 1 });
